0a589753050cacd82aced57b04d3b47788043654
Jonathan Cockrem <jctotalportal@gmail.com>
2021-12-16 18:31:05 -0500

    Initial commit. Built project. Still runs

---
+++ bugedex/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BugComponent } from './bug/bug/bug.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BugComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NoopAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
