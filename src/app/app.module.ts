b53e5b2b3626e01d533e802dc6c4755780dd2a5f
Jonathan Cockrem <jctotalportal@gmail.com>
2022-02-01 17:26:07 -0500

    Structure update. Fixing some node issues

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BugComponent } from './bug/bug.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatCommonModule } from '@angular/material/core';
+import { BugDetailsComponent } from './bug/bug-details/bug-details/bug-details.component';
+import { MatCardModule } from '@angular/material/card'
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BugComponent,
+    BugDetailsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatTableModule,
+    HttpClientModule,
+    MatCommonModule,
+    MatCardModule,
+    MatCheckboxModule
+  ],
+  providers: [HttpClient],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/bug/bug-details/bug-details/bug-details.component.spec.ts
@@ -0,0 +1,42 @@
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { createComponentFactory, Spectator, SpyObject } from '@ngneat/spectator';
+import { BugService } from '../../shared/bug.service';
+import { BugDetailsComponent } from './bug-details.component';
+import faker from 'faker';
+import { Bug } from '../../shared/models/bug.model';
+import { Month } from '../../shared/models/month.model';
+import { BehaviorSubject, of } from 'rxjs';
+
+describe('BugDetailsComponent', () => {
+  let spectator: Spectator<BugDetailsComponent>;
+  let component: BugDetailsComponent;
+  let bugService: SpyObject<BugService>;
+ //let fakeBugs: Bug[] = [];
+
+  const createComponent = createComponentFactory({
+      component: BugDetailsComponent,
+      imports: [
+          RouterTestingModule.withRoutes([{ path: 'bug', component: BugDetailsComponent}])
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { queryParams: { name: "T"}}}
+        }
+      ],
+      detectChanges: false,
+      mocks: [ BugService ]
+    });
+
+  beforeEach( () => {
+    spectator = createComponent();
+    component = spectator.component;
+    bugService = spectator.inject(BugService);
+    bugService.state = new BehaviorSubject<any>([])
+  });
+
+  it('should create', () => {
+    expect(spectator.component).toBeTruthy();
+  });  
+});
