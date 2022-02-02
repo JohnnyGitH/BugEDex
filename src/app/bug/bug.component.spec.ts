4a6bb4f5e4f762ccfd35b0c2ccfe9ad9531d7e10
Jonathan Cockrem <jctotalportal@gmail.com>
2022-02-02 13:05:56 -0500

    Update bug.component.spec.ts
    push up to save this. Still need to fix.

--- src/app/bug/bug.component.spec.ts
+++ src/app/bug/bug.component.spec.ts
@@ -6,6 +6,7 @@ import { BugService } from './shared/bug.service';
 import { Month } from './shared/models/month.model';
 import { Bug } from './shared/models/bug.model';
 import { BehaviorSubject, of } from 'rxjs';
+import faker from 'faker';
 
 describe('BugComponent', () => {
   let spectator: Spectator<BugComponent>;
@@ -29,6 +30,20 @@ describe('BugComponent', () => {
       mocks:[ BugService ]
   });
 
+  const createFakeBugModel = (): Bug => {
+    return {
+      name: "butterfly",
+      location: "location",
+      time: "time",
+      price: 1,
+      month:{
+        north:[],
+        south:[]
+      } as Month,
+      caught: false
+    }
+  }
+
   beforeEach (() => {
     spectator = createComponent();
     component = spectator.component;
@@ -46,7 +61,7 @@ describe('BugComponent', () => {
 
     for(let i=0;i<10;i++){
       bugArray.push({
-        name:"buterfly"+i,
+        name:"butterfly"+i,
         location:"location"+i,
         time:"time"+i,
         price: 10+i,
@@ -66,40 +81,42 @@ describe('BugComponent', () => {
     })
     // Get bug array, use a name, check its caught status
     // then run checkBugCaught(), check if the bugname is updated.
-    describe("checkBugCaught()", ()=>{
-      let bugArray = [];
-      //Add a single Bug to the array
-      for(let i=0;i<1;i++){
-        bugArray.push({
-          name:"butterfly"+i,
-          location:"location"+i,
-          time:"time"+i,
-          price: 10+i,
-          month:{
-            north:[],
-            south:[]
-          } as Month,
-          caught:false
-        } as Bug);
-      }
+    fdescribe("checkBugCaught()", ()=>{
+      let bArray: Bug[] = [];
+      let afterArray: Bug[] = [];
+      let bug: Bug;
+      bug = createFakeBugModel();
+      bArray.push(bug);
 
       it("should find the bug being selected and update the caught property", () => {
-        let bugName = "butterfly0";
-        console.log(bugArray);
-        expect(bugArray).toHaveData;
-        foundBug = bugArray[0];
-        console.log(foundBug);
-        console.log(foundBug.name);
-        console.log(foundBug.caught);
+        let bugName = bug.name;
+        let foundBug: Bug;
+        foundBug = { caught: false, name: "", location: "", time: "", price: 1, month: {north: [],south: [],},};
+        let inputArrayBug: Bug;
+        let expectedArrayBug: Bug;
+        // Make sure Array has bug
+        expect(bArray).toHaveData;
 
-        bugService.state = new BehaviorSubject<any>(of(bugArray));
+        inputArrayBug = bArray[0];
+
+        //Assign caught / name
+        foundBug.caught = inputArrayBug.caught;
+        foundBug.name = inputArrayBug.name;
+
+        console.log(foundBug);
+
+        bugService.state = new BehaviorSubject<any>(of(bArray));
 
         expect(foundBug.name).toHaveText(bugName);
         expect(foundBug.caught).toBeFalsy;
-
+  
         spectator.component.checkBugCaught(bugName);
-        bugArray = bugService.state.getValue();
-        foundBug = bugArray[0]
+        afterArray = bugService.state.getValue(); // failing here.
+
+        console.log(afterArray);
+
+        expectedArrayBug = afterArray[0];
+        foundBug.caught = expectedArrayBug.caught
 
         expect(foundBug.caught).toBeTruthy;
       });
