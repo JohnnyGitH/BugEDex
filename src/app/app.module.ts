ba758d851f155824ef1fc01f4305d43ca61b2fe5
Johnny C <joncockrem@gmail.com>
2025-12-11 23:35:31 -0500

    Update to Angular 17 a success, builds, starts and runs the same

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatLegacyTableModule as MatTableModule } from '@angular/material/legacy-table'
+import { MatTableModule as MatTableModule } from '@angular/material/table'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BugComponent } from './bug/bug.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { MatCommonModule } from '@angular/material/core';
 import { BugDetailsComponent } from './bug/bug-details/bug-details/bug-details.component';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card'
-import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
+import { MatCardModule as MatCardModule } from '@angular/material/card'
+import { MatCheckboxModule as MatCheckboxModule } from '@angular/material/checkbox';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 
 @NgModule({
